2c07e05c4d8dfff034d3a6d270a4feedccfcccb6
Naze-na <nabeel.nazeer7@outlook.com>
2026-06-18 01:44:49 +0000

    new bash scripting files

---
+++ new/print.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo Hello my name is Nabeel 
+echo Nabeel is currently studing linux 
+echo Nabeel loves DevOps
+echo 26*30*12
---
+++ new/variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo Hello my name is $name
+echo $name is currently studying linux
+echo $name loves DevOps
+
+name=Tim
